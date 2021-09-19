73dd7174384cc8a0e59fdd560d76c968eb78e0e6
mksci <mikoowsky@gmail.com>
2021-09-19 12:19:50 +0200

    add_task_component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import { useState } from "react"
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Wake up',
+      hour: '8:30',
+    },
+    {
+      id:2,
+      text: 'Office',
+      hour: '9:00',
+    },
+    {
+      id:3,
+      text: 'Party',
+      hour: '18:00',
+    },
+  ])
+
   return (
     <div className="container">
      <Header title='Next task'/>
+     <Tasks tasks={tasks}/>
     </div>    
   );
 }
+
  
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+const Tasks = ({ tasks }) => {
+    return (
+        <>
+        {tasks.map((task) => (
+            <h3>{task.text}</h3>
+            ))}
+        </>
+    )
+}
+
+
+export default Tasks
